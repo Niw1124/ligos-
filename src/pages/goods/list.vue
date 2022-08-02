ae6a60b93a9a5fa09c2b937e946010a16a2f1d0e
Niw1124 <1293003584@qq.com>
2022-08-02 23:31:24 +0800

    商品模块自定义表格商品列表样式

--- src/pages/goods/list.vue
+++ src/pages/goods/list.vue
@@ -41,45 +41,79 @@
         style="width: 100%"
         v-loading="loading"
       >
-        <el-table-column label="管理员" width="200">
+        <el-table-column label="商品" width="300" align="center">
           <template #default="{ row }">
-            <div class="flex items-center">
-              <el-avatar :size="40" :src="row.avatar">
-                <img
-                  src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png"
-                />
-              </el-avatar>
-              <div class="ml-3">
-                <h6>{{ row.username }}</h6>
-                <small>ID:{{ row.id }}</small>
+            <div class="flex">
+              <el-image
+                :src="row.cover"
+                fit="cover"
+                :lazy="true"
+                style="width: 50px; height: 50px"
+                class="mr-3"
+              ></el-image>
+              <div class="flex-1">
+                <p>{{ row.title }}</p>
+                <div>
+                  <span class="text-rose-500">￥{{ row.min_price }}</span>
+                  <el-divider direction="vertical"></el-divider>
+                  <span class="text-gray-500 text-xs"
+                    >￥{{ row.min_oprice }}</span
+                  >
+                </div>
+                <p class="text-gray-400 text-xs mb-1">
+                  分类：{{ row.category ? row.category.name : "未分类" }}
+                </p>
+                <p class="text-gray-400 text-xs">
+                  创建时间：{{ row.create_time }}
+                </p>
               </div>
             </div>
           </template>
         </el-table-column>
-        <el-table-column label="所属角色" align="center">
+        <el-table-column
+          label="实际销量"
+          prop="sale_count"
+          align="center"
+          width="80"
+        />
+
+        <el-table-column label="商品状态" width="100" align="center">
           <template #default="{ row }">
-            <div>{{ row.role ? row.role.name : "-" }}</div>
-          </template>
-        </el-table-column>
-        <el-table-column label="状态" width="120">
-          <template #default="{ row }">
-            <el-switch
-              :modelValue="row.status"
-              :active-value="1"
-              :inactive-value="0"
-              @change="handleStatisChange($event, row)"
-              :loading="row.statusLoading"
-              :disabled="row.super == 1"
+            <el-tag
+              :type="row.status == 0 ? 'success' : 'danger'"
+              size="small"
+              text
+              >{{ row.status == 0 ? "上架" : "仓库" }}</el-tag
             >
-            </el-switch>
           </template>
         </el-table-column>
-        <el-table-column label="操作" width="180" align="center">
+        <el-table-column
+          label="审核状态"
+          width="120"
+          align="center"
+          v-if="searchForm.tab != 'delete'"
+        >
+          <template #default="{ row }">
+            <div class="flex flex-col" v-if="row.ischeck == 0">
+              <el-button type="success" size="small" plain>审核通过</el-button>
+              <el-button type="danger" size="small" plain class="mt-2 !ml-0"
+                >审核拒绝</el-button
+              >
+            </div>
+            <span v-else>{{
+              row.ischeck == 1 ? "审核通过" : "审核不通过"
+            }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="总库存"
+          prop="stock"
+          align="center"
+          width="90"
+        />
+        <el-table-column label="操作" align="center">
           <template #default="scope">
-            <small v-if="scope.row.super == 1" class="text-sm text-gray-500"
-              >暂无操作</small
-            >
-            <div v-else>
+            <div v-if="searchForm.tab != 'delete'">
               <el-button
                 type="primary"
                 size="small"
@@ -87,8 +121,17 @@
                 @click="handleEdit(scope.row)"
                 >修改</el-button
               >
+              <el-button class="px-1" type="primary" size="small" text
+                >商品规格</el-button
+              >
+              <el-button class="px-1" type="primary" size="small" text
+                >设置轮播图</el-button
+              >
+              <el-button class="px-1" type="primary" size="small" text
+                >商品详情</el-button
+              >
               <el-popconfirm
-                title="你确定要删除该管理员？"
+                title="你确定要删除该商品？"
                 confirmButtonText="确认"
                 cancelButtonText="取消"
                 @confirm="handleDelete(scope.row.id)"
@@ -98,6 +141,7 @@
                 </template>
               </el-popconfirm>
             </div>
+            <span v-else>暂停操作</span>
           </template>
         </el-table-column>
       </el-table>
@@ -253,4 +297,4 @@ const tabbars = [
 ];
 </script>
 
-<style scoped></style>
+<style></style>
