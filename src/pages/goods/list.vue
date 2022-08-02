5906257b9265fa4906e6dbe9c130d9d79e149c57
Niw1124 <1293003584@qq.com>
2022-08-02 21:41:11 +0800

    商品模块tabs标签页开发

--- src/pages/goods/list.vue
+++ src/pages/goods/list.vue
@@ -1,142 +1,160 @@
 <template>
-  <el-card shadow="never" class="border-0">
-    <!-- 搜索 -->
-    <el-form :model="searchForm" label-width="80px" class="mb-3">
-      <el-row :gutter="20">
-        <el-col :span="8" :offset="0">
-          <el-form-item label="关键词">
-            <el-input
-              v-model="searchForm.keyword"
-              placeholder="管理员昵称"
-              clearable
-            ></el-input> </el-form-item
-        ></el-col>
-        <el-col :span="8" :offset="8">
-          <div class="flex items-center justify-end">
-            <el-button type="primary" @click="getData">搜索</el-button>
-            <el-button @click="resetSearchForm">重置</el-button>
-          </div></el-col
-        >
-      </el-row>
-    </el-form>
-
-    <!-- 新增和刷新 -->
-    <list-header @refresh="getData" @increment="handleCreateForm"></list-header>
-    <!-- 表格 -->
-    <el-table :data="tableData" stripe style="width: 100%" v-loading="loading">
-      <el-table-column label="管理员" width="200">
-        <template #default="{ row }">
-          <div class="flex items-center">
-            <el-avatar :size="40" :src="row.avatar">
-              <img
-                src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png"
-              />
-            </el-avatar>
-            <div class="ml-3">
-              <h6>{{ row.username }}</h6>
-              <small>ID:{{ row.id }}</small>
-            </div>
-          </div>
-        </template>
-      </el-table-column>
-      <el-table-column label="所属角色" align="center">
-        <template #default="{ row }">
-          <div>{{ row.role ? row.role.name : "-" }}</div>
-        </template>
-      </el-table-column>
-      <el-table-column label="状态" width="120">
-        <template #default="{ row }">
-          <el-switch
-            :modelValue="row.status"
-            :active-value="1"
-            :inactive-value="0"
-            @change="handleStatisChange($event, row)"
-            :loading="row.statusLoading"
-            :disabled="row.super == 1"
+  <div>
+    <el-tabs v-model="searchForm.tab" @tab-change="getData">
+      <el-tab-pane
+        :label="item.name"
+        :name="item.key"
+        v-for="(item, index) in tabbars"
+        :key="index"
+      ></el-tab-pane>
+    </el-tabs>
+    <el-card shadow="never" class="border-0">
+      <!-- 搜索 -->
+      <el-form :model="searchForm" label-width="80px" class="mb-3">
+        <el-row :gutter="20">
+          <el-col :span="8" :offset="0">
+            <el-form-item label="关键词">
+              <el-input
+                v-model="searchForm.title"
+                placeholder="商品名称"
+                clearable
+              ></el-input> </el-form-item
+          ></el-col>
+          <el-col :span="8" :offset="8">
+            <div class="flex items-center justify-end">
+              <el-button type="primary" @click="getData">搜索</el-button>
+              <el-button @click="resetSearchForm">重置</el-button>
+            </div></el-col
           >
-          </el-switch>
-        </template>
-      </el-table-column>
-      <el-table-column label="操作" width="180" align="center">
-        <template #default="scope">
-          <small v-if="scope.row.super == 1" class="text-sm text-gray-500"
-            >暂无操作</small
-          >
-          <div v-else>
-            <el-button
-              type="primary"
-              size="small"
-              text
-              @click="handleEdit(scope.row)"
-              >修改</el-button
-            >
-            <el-popconfirm
-              title="你确定要删除该管理员？"
-              confirmButtonText="确认"
-              cancelButtonText="取消"
-              @confirm="handleDelete(scope.row.id)"
-            >
-              <template #reference>
-                <el-button type="primary" size="small" text>删除</el-button>
-              </template>
-            </el-popconfirm>
-          </div>
-        </template>
-      </el-table-column>
-    </el-table>
-    <div class="flex items-center justify-center mt-5">
-      <el-pagination
-        background
-        layout="prev, pager,next"
-        :total="total"
-        :current-page="currentPage"
-        :page-size="limit"
-        @current-change="getData"
-      />
-    </div>
-    <form-component
-      ref="FormComponentRef"
-      :title="drawTitle"
-      @submit="handleSubmit"
-    >
-      <el-form
-        :model="form"
-        ref="formRef"
-        :rules="rules"
-        label-width="80px"
-        :inline="false"
-      >
-        <el-form-item label="用户名" prop="username">
-          <el-input v-model="form.username" placeholder="用户名"></el-input>
-        </el-form-item>
-        <el-form-item label="密码" prop="password">
-          <el-input v-model="form.password" placeholder="用户密码"></el-input>
-        </el-form-item>
-        <el-form-item label="头像" prop="avatar">
-          <choose-image v-model="form.avatar"></choose-image>
-        </el-form-item>
-        <el-form-item label="所属角色" prop="role_id">
-          <el-select v-model="form.role_id" placeholder="选择所属角色">
-            <el-option
-              v-for="item in roles"
-              :key="item.id"
-              :label="item.name"
-              :value="item.id"
-            >
-            </el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="状态" prop="status">
-          <el-switch
-            v-model="form.status"
-            :active-value="1"
-            :inactive-value="0"
-          >
-          </el-switch>
-        </el-form-item>
+        </el-row>
       </el-form>
-    </form-component>
-  </el-card>
+
+      <!-- 新增和刷新 -->
+      <list-header
+        @refresh="getData"
+        @increment="handleCreateForm"
+      ></list-header>
+      <!-- 表格 -->
+      <el-table
+        :data="tableData"
+        stripe
+        style="width: 100%"
+        v-loading="loading"
+      >
+        <el-table-column label="管理员" width="200">
+          <template #default="{ row }">
+            <div class="flex items-center">
+              <el-avatar :size="40" :src="row.avatar">
+                <img
+                  src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png"
+                />
+              </el-avatar>
+              <div class="ml-3">
+                <h6>{{ row.username }}</h6>
+                <small>ID:{{ row.id }}</small>
+              </div>
+            </div>
+          </template>
+        </el-table-column>
+        <el-table-column label="所属角色" align="center">
+          <template #default="{ row }">
+            <div>{{ row.role ? row.role.name : "-" }}</div>
+          </template>
+        </el-table-column>
+        <el-table-column label="状态" width="120">
+          <template #default="{ row }">
+            <el-switch
+              :modelValue="row.status"
+              :active-value="1"
+              :inactive-value="0"
+              @change="handleStatisChange($event, row)"
+              :loading="row.statusLoading"
+              :disabled="row.super == 1"
+            >
+            </el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="180" align="center">
+          <template #default="scope">
+            <small v-if="scope.row.super == 1" class="text-sm text-gray-500"
+              >暂无操作</small
+            >
+            <div v-else>
+              <el-button
+                type="primary"
+                size="small"
+                text
+                @click="handleEdit(scope.row)"
+                >修改</el-button
+              >
+              <el-popconfirm
+                title="你确定要删除该管理员？"
+                confirmButtonText="确认"
+                cancelButtonText="取消"
+                @confirm="handleDelete(scope.row.id)"
+              >
+                <template #reference>
+                  <el-button type="primary" size="small" text>删除</el-button>
+                </template>
+              </el-popconfirm>
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="flex items-center justify-center mt-5">
+        <el-pagination
+          background
+          layout="prev, pager,next"
+          :total="total"
+          :current-page="currentPage"
+          :page-size="limit"
+          @current-change="getData"
+        />
+      </div>
+      <form-component
+        ref="FormComponentRef"
+        :title="drawTitle"
+        @submit="handleSubmit"
+      >
+        <el-form
+          :model="form"
+          ref="formRef"
+          :rules="rules"
+          label-width="80px"
+          :inline="false"
+        >
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="form.username" placeholder="用户名"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input v-model="form.password" placeholder="用户密码"></el-input>
+          </el-form-item>
+          <el-form-item label="头像" prop="avatar">
+            <choose-image v-model="form.avatar"></choose-image>
+          </el-form-item>
+          <el-form-item label="所属角色" prop="role_id">
+            <el-select v-model="form.role_id" placeholder="选择所属角色">
+              <el-option
+                v-for="item in roles"
+                :key="item.id"
+                :label="item.name"
+                :value="item.id"
+              >
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="状态" prop="status">
+            <el-switch
+              v-model="form.status"
+              :active-value="1"
+              :inactive-value="0"
+            >
+            </el-switch>
+          </el-form-item>
+        </el-form>
+      </form-component>
+    </el-card>
+  </div>
 </template>
 
 <script setup>
@@ -165,7 +183,9 @@ const {
   handleStatisChange,
 } = useInitTable({
   searchForm: {
-    keyword: "",
+    title: "",
+    tab: "all",
+    category_id: null,
   },
   delete: deleteGoods,
   updateStatus: updatedGoodsStatus,
@@ -204,6 +224,33 @@ const {
 });
 
 const roles = ref([]);
+//订单类型
+const tabbars = [
+  {
+    key: "all",
+    name: "全部",
+  },
+  {
+    key: "checking",
+    name: "审核中",
+  },
+  {
+    key: "saling",
+    name: "出售中",
+  },
+  {
+    key: "off",
+    name: "已下架",
+  },
+  {
+    key: "min_stock",
+    name: "库存预警",
+  },
+  {
+    key: "delete",
+    name: "回收站",
+  },
+];
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
