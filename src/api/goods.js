c15023c9e0419aff8dd136b0a5a544193e93339c
Niw1124 <1293003584@qq.com>
2022-08-02 21:14:20 +0800

    商品模块复用代码实现

---
+++ src/api/goods.js
@@ -0,0 +1,19 @@
+import axios from "~/axios";
+import { queryParams } from "~/tools/useUrlCode.js";
+export function getGoodsList(page, query = {}) {
+  let r = queryParams(query);
+
+  return axios.get(`/admin/goods/${page}${r}`);
+}
+export function updatedGoodsStatus(ids, status) {
+  return axios.post(`/admin/goods/changestatus`, { ids, status });
+}
+export function createGoods(data) {
+  return axios.post("/admin/goods", data);
+}
+export function updateGoods(id, data) {
+  return axios.post(`/admin/goods/${id}`, data);
+}
+export function deleteGoods(ids) {
+  return axios.post(`/admin/goods/delete_all`, { ids });
+}
