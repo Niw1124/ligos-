b4e1ca40341d96a9da1bc12680fc30fe7a6f471a
Niw1124 <1293003584@qq.com>
2022-08-04 21:34:05 +0800

    商品模块富文本编辑器 --自定义按钮的初步实现

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -61,7 +61,7 @@ const init = {
   plugins:
     "wordcount visualchars visualblocks template searchreplace save quickbars preview pagebreak nonbreaking media insertdatetime importcss image help fullscreen directionality codesample code charmap link code table lists advlist anchor autolink autoresize autosave",
   toolbar:
-    "formats undo redo fontsizeselect fontselect ltr rtl searchreplace media|outdent indent aligncenter alignleft alignright alignjustify lineheight underline quicklink h2 h3 blockquote numlist bullist table removeformat forecolor backcolor bold italic strikethrough hr link preview fullscreen help ",
+    "formats undo redo  fontsizeselect fontselect ltr rtl searchreplace media imageUpload|outdent indent aligncenter alignleft alignright alignjustify lineheight underline quicklink h2 h3 blockquote numlist bullist table removeformat forecolor backcolor bold italic strikethrough hr link preview fullscreen help ",
   content_style: "p {margin: 5px 0; font-size: 14px}",
   fontsize_formats: "12px 14px 16px 18px 24px 36px 48px 56px 72px",
   font_formats:
@@ -70,6 +70,17 @@ const init = {
   elementpath: false,
   resize: false, // 禁止改变大小
   statusbar: false, // 隐藏底部状态栏
+  setup: (editor) => {
+    editor.ui.registry.addButton("imageUpload", {
+      tooltip: "插入图片", //指上去的提示文字
+      icon: "image",
+      onAction() {
+        editor.insertContent(
+          `<img src="http://tangzhe123-com.oss-cn-shenzhen.aliyuncs.com/public/62af04afe39fa.jpg"  style="width:100%" />`
+        );
+      },
+    });
+  },
 };
 tinymce.init; // 初始化
 const content = ref(props.modelValue);
